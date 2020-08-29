b1d3e6376ebb09cc742acfd9e2ac4ebf2912d625
ankit thakur <ankit.dl@live.com>
2020-08-29 16:10:41 +0530

    third commit

--- app/src/main/java/com/ankit/gitrevertdemo/MainActivity.java
+++ app/src/main/java/com/ankit/gitrevertdemo/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("changes","second  commit changes");
+        Log.i("changes","third  commit changes");
+
     }
 }
