15d2233f312dd5eee5d8ead0f33759b609a836fc
ankit thakur <ankit.dl@live.com>
2020-08-30 00:16:57 +0530

    first commit by friend

--- app/src/main/java/com/ankit/gitrevertdemo/MainActivity.java
+++ app/src/main/java/com/ankit/gitrevertdemo/MainActivity.java
@@ -6,14 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    String F_TAG = "Thakur";
+    String TAG = "Ankit";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("changes","second  commit changes");
-        Log.i("changes","third  commit changes");
-        Log.i("changes","first  commit changes - otherbranch");
+        Log.i("changes", "second  commit changes");
+        Log.i("changes", "third  commit changes");
+        Log.i("changes", "first  commit changes - otherbranch");
+        Log.i(F_TAG, "first  commit changes - by Friend");
+
 
     }
 }
